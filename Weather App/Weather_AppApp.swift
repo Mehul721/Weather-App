ca86f26af4d1cde3a8a559fc62b937daf7fac71c
Mehul721 <jmehul721@gmail.com>
2024-07-11 22:58:41 +0530

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Mehul Jain on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
